b8ab2afc9e81c0b62930db01b61338931c27a09e
Jake Churchill <jake.churchill@nrel.gov>
2025-11-24 18:14:05 -0500

    Formating and naming updates for decarbonator

--- src/wrd/components/decarbonator.py
+++ src/wrd/components/decarbonator.py
@@ -35,11 +35,14 @@ from watertap.core.solvers import get_solver
 from watertap.core import Database
 from watertap.unit_models.zero_order import ChemicalAdditionZO
 from watertap.core.wt_database import Database
-from watertap.core.zero_order_properties import WaterParameterBlock
+
+# from watertap.core.zero_order_properties import WaterParameterBlock
 from watertap.core.util.model_diagnostics.infeasible import *
-from watertap.costing.zero_order_costing import ZeroOrderCosting
+
+# from watertap.costing.zero_order_costing import ZeroOrderCosting
 from watertap.core.util.initialization import *
 from watertap.property_models.NaCl_T_dep_prop_pack import NaClParameterBlock
+
 
 def build_system(**kwargs):
     m = ConcreteModel()
@@ -48,6 +51,7 @@ def build_system(**kwargs):
     m.fs.decarb_system = FlowsheetBlock(dynamic=False)
     build_decarbonator(m.fs.decarb_system, prop_package=m.fs.ro_properties, **kwargs)
     return m
+
 
 def build_decarbonator(blk, prop_package):
 
@@ -81,7 +85,7 @@ def build_decarbonator(blk, prop_package):
 
 
 # This function should have different default test values, but it doesn't do any thing really...
-def set_inlet_conditions(blk, Qin=.5*0.154, Cin=2*0.542, P_in=10.6):
+def set_inlet_conditions(blk, Qin=0.5 * 0.154, Cin=2 * 0.542, P_in=10.6):
     """
     Set the operation conditions for the RO stage
     """
@@ -97,31 +101,38 @@ def set_inlet_conditions(blk, Qin=.5*0.154, Cin=2*0.542, P_in=10.6):
     blk.feed.properties[0].pressure.fix(P_in * pyunits.bar)
     blk.feed.properties[0].flow_vol  # Touching
 
-def set_decarb_op_conditions(blk):
+
+def set_decarbonator_op_conditions(blk):
     # Instead of hard coding value, this should be in some yaml file, but it doesn't need one of its own
-    blk.unit.power_consumption.fix(2.5*pyunits.kW)
+    blk.unit.power_consumption.fix(2.5 * pyunits.kW)
     # Decarb fit params: y = mx + b
     # blk.unit.power_eq = Constraint(
-    #     expr = blk.unit.power_consumption == m * blk.unit.feed.properties[0].flow_vol + b 
+    #     expr = blk.unit.power_consumption == m * blk.unit.feed.properties[0].flow_vol + b
     # )
 
-def add_decarb_scaling(blk):
+
+def add_decarbonator_scaling(blk):
     set_scaling_factor(blk.unit.power_consumption, 1e-3)
 
-def initialize_decarb(blk):
+
+def initialize_decarbonator(blk):
     blk.feed.initialize()
     propagate_state(blk.feed_to_decarb)
     blk.unit.initialize()
     propagate_state(blk.decarb_to_product)
     blk.product.initialize()
 
+
 def cost_decarbonator(blk):
-    lb = blk.unit.power_consumption.lb # Not sure why this is here, but it was in pump costing method
+    lb = (
+        blk.unit.power_consumption.lb
+    )  # Not sure why this is here, but it was in pump costing method
     blk.unit_model.work_mechanical.setlb(0)
-    blk.costing_package.cost_flow(blk.unit.power_consumption,"electricity")
+    blk.costing_package.cost_flow(blk.unit.power_consumption, "electricity")
     blk.unit.power_consumption.setlb(lb)
 
-def report_decarb(blk, w=30):
+
+def report_decarbonator(blk, w=30):
     title = "Pump Report"
     side = int(((3 * w) - len(title)) / 2) - 1
     header = "=" * side + f" {title} " + "=" * side
@@ -142,14 +153,16 @@ def report_decarb(blk, w=30):
         f'{f"Power Consumption (kW)":<{w}s}{value(pyunits.convert(power, to_units=pyunits.kW)):<{w}.3f}{"kW"}'
     )
 
+
 if __name__ == "__main__":
     m = build_system()  # optional input of stage_num
     print(f"{degrees_of_freedom(m)} degrees of freedom after build")
     set_inlet_conditions(m.fs.decarb_system, Qin=0.154, Cin=0.542, P_in=1)
-    set_decarb_op_conditions(m.fs.decarb_system)
+    set_decarbonator_op_conditions(m.fs.decarb_system)
     print(f"{degrees_of_freedom(m)} degrees of freedom after setting op conditions")
-    add_decarb_scaling(m.fs.decarb_system)
-    initialize_decarb(m.fs.decarb_system)
+    add_decarbonator_scaling(m.fs.decarb_system)
+    initialize_decarbonator(m.fs.decarb_system)
+    # cost_decarbonator(m.fs.decarb_system) # Haven't done any costing yet
     m.fs.obj = Objective(
         expr=m.fs.decarb_system.feed.properties[0].flow_vol_phase["Liq"]
     )  # There is no D.o.f to optimize with
@@ -157,4 +170,4 @@ if __name__ == "__main__":
     results = solver.solve(m)
     assert_optimal_termination(results)
     # print(f"{iscale.jacobian_cond(m.fs.decarb_system):.2e}")
-    report_decarb(m.fs.decarb_system)
+    report_decarbonator(m.fs.decarb_system)
