4a4e9c95abc5b3a99481140382cfe0b27dcc8bdd
kurbansitterley <kurban.sitterley@nrel.gov>
2025-12-16 17:10:24 -0700

    black

--- src/wrd/tests/test_decarbonator.py
+++ src/wrd/tests/test_decarbonator.py
@@ -2,10 +2,10 @@ import pytest
 
 from wrd.components.decarbonator import main
 
+
 @pytest.mark.component
 def test_decarbonator():
     m = main()
     assert (
-        pytest.approx(m.fs.decarb_system.unit.power_consumption.value, rel=1e-3)
-        == 2.5
+        pytest.approx(m.fs.decarb_system.unit.power_consumption.value, rel=1e-3) == 2.5
     )
