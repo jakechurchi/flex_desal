fb8b1e992b50a04d7ae6d3623d8d0fba3ddc2ca8
Jake Churchill <jake.churchill@nrel.gov>
2026-05-04 15:05:28 -0600

    black

--- src/wrd/components/ro.py
+++ src/wrd/components/ro.py
@@ -278,17 +278,18 @@ def set_ro_op_conditions(blk):
     )
 
     blk.unit.mixed_permeate[0].pressure.fix(20 * pyunits.psi)
-    
-    # Adding estimate of first element flux as an output to report 
+
+    # Adding estimate of first element flux as an output to report
     blk.unit.first_elem_prod = Expression(
-        expr = blk.unit.flux_mass_phase_comp[0,0.1,"Liq", "H2O"] *
-            get_config_value(
-                blk.config_data,
-                "element_membrane_area",
-                "reverse_osmosis_1d",
-                f"stage_{blk.stage_num}"
-                ) / blk.feed.properties[0].dens_mass_phase["Liq"]
+        expr=blk.unit.flux_mass_phase_comp[0, 0.1, "Liq", "H2O"]
+        * get_config_value(
+            blk.config_data,
+            "element_membrane_area",
+            "reverse_osmosis_1d",
+            f"stage_{blk.stage_num}",
         )
+        / blk.feed.properties[0].dens_mass_phase["Liq"]
+    )
 
 
 def initialize_system(m):
@@ -358,7 +359,6 @@ def report_ro(blk, w=30):
     print(f'{"Parameter":<{w}s}{"Value":<{w}s}{"Units":<{w}s}')
     print(f"{'-' * (3 * w)}")
 
-
     print(
         f'{f"Inlet Flow":<{w}s}{value(pyunits.convert(blk.feed.properties[0].flow_vol_phase["Liq"], to_units=pyunits.gallon / pyunits.minute)):<{w}.3f}{"gpm"}'
     )
@@ -401,6 +401,7 @@ def report_ro(blk, w=30):
     print(
         f'{f"1st Elem. Perm. Flow":<{w}s}{value(pyunits.convert(blk.unit.first_elem_prod, to_units=pyunits.m**3 / pyunits.hour)):<{w}.3f}{f"m3/hr"}'
     )
+
 
 def add_ro_costing(blk, costing_package=None):
 
